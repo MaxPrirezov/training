cc086244aa100e047d5bb4c26f336952d13a85da
Max <maxprirezov7@gmail.com>
2022-11-17 19:51:14 +0600

    2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
+let num = 55;
 
-const a = prompt("Какой проследный фильм вы смотрели?", ""),
-      b = prompt("От 1 до 10 как вам этот фильм?", ""),
-      c = prompt("Какой предпоследний фильм вы смотрели?", ""),
-      d = prompt("От 1 до 10 как вам этот фильм?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genders: [],
-  privat: false,
-};
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+while (num < 60) {
+  num++;
+  console.log(num);
+}
+
+do {
+  num++;
+  console.log(num);
+} while (num < 60);
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
