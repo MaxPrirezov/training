5aafcf9545a7efbd44d79e456c70b865c1b9f65d
Max <maxprirezov7@gmail.com>
2022-11-17 21:24:12 +0600

    3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-let num = 55;
+"use strict";
 
-while (num < 60) {
-  num++;
-  console.log(num);
+let numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
+
+while (
+  numberOfFilms == "" ||
+  numberOfFilms == null ||
+  numberOfFilms.length > 50
+) {
+  numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
 }
-
-do {
-  num++;
-  console.log(num);
-} while (num < 60);
-
-for (var i = 0; i < 10; i++) {
-  console.log(i);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genders: [],
+  privat: false,
+};
+if (personalMovieDB.count < 10) {
+  alert("Вы мал");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("вы класс");
+} else if (personalMovieDB.count > 30) {
+  alert("вы киноман");
+} else {
+  alert("вы ошибка");
 }
+for (var i = 1; i <= 2; i++) {
+  let a = prompt(`Какой ${i} фильм вы смотрели?`, "");
+  a == "" || a == null || a.length > 50
+    ? (a = prompt(`Какой ${i} фильм вы смотрели?`, ""))
+    : console.log("успех");
+  let b;
+  do {
+    b = prompt(`От 1 до 10 как вам ${i} фильм?`, "");
+  } while (b == "" || b == null || b.length > 50);
+  personalMovieDB.movies[a] = b;
+}
+console.log(personalMovieDB);
